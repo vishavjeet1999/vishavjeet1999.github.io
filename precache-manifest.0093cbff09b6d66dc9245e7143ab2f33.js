1d770e96ee83ed848e4db70770885956ed72c83a
vishavjeet1999 <52985689+vishavjeet1999@users.noreply.github.com>
2020-05-09 15:57:00 +0530

    Updates

---
+++ precache-manifest.0093cbff09b6d66dc9245e7143ab2f33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f00c763a2ea0aa0cc18320f41c2e83",
+    "url": "/facedetector/index.html"
+  },
+  {
+    "revision": "b7e8516a0f6c9d777c3a",
+    "url": "/facedetector/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "9c8c52365ff762637a34",
+    "url": "/facedetector/static/css/main.951fa782.chunk.css"
+  },
+  {
+    "revision": "b7e8516a0f6c9d777c3a",
+    "url": "/facedetector/static/js/2.505285e1.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/facedetector/static/js/2.505285e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8c52365ff762637a34",
+    "url": "/facedetector/static/js/main.fbd98681.chunk.js"
+  },
+  {
+    "revision": "3e988a11a4c3070433e3",
+    "url": "/facedetector/static/js/runtime-main.53f0ba78.js"
+  }
+]);
